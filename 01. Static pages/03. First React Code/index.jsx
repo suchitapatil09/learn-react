b945961da26b672a6d3f20ec5cc03c97ed5e0f45
suchitapatil09 <suchitar.cs24@bmsce.ac.in>
2025-10-05 16:02:11 +0530

    Update index.jsx

--- 01. Static pages/03. First React Code/index.jsx
+++ 01. Static pages/03. First React Code/index.jsx
@@ -1,4 +1,6 @@
-// Challenge: Re-write the first lines of React code we just had.
+//Challenge: Re-write the first lines of React code we just had.
 // You can render anything you want to the screen, doesn't have
 // to be an h1 element.
-
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+root.render(<h1> Max Verstappen </h1>)
